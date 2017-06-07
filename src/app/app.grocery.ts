ec92826c760092ea013c7fce589f04c2eb766014
royagasthyan <royagasthyan@gmail.com>
2017-06-07 22:05:24 +0530

    Update app.grocery.ts

--- src/app/app.grocery.ts
+++ src/app/app.grocery.ts
@@ -9,35 +9,49 @@ import { Component } from '@angular/core';
 })
 
 export class GroceryComponent {
-  task = {
-	name: '',
-	id: 0
-  };
-  tasks = [];
+    task = {
+      name: '',
+      id: 0
+    };
+    tasks = [];
 
-  onClick(){
-	if(this.task.id == 0){
-		this.tasks.push({id: (new Date()).getTime(),name: this.task.name, strike: false});
-	}
-	
-  	this.task = {
-		name: '',
-		id: 0
-	};
-  }
-  
-  onEdit(item){
-	this.task = item;
-  }
-  
-  onDelete(item){
-	for(var i = 0;i < this.tasks.length; i++){
-		if(item.id == this.tasks[i].id){
-			this.tasks.splice(i,1);
-			break;
-		}
-	}
-  }
+    onClick(){
+      if(this.task.id == 0){
+        this.tasks.push({id: (new Date()).getTime(),name: this.task.name, strike: false});
+      }
+      
+      this.task = {
+        name: '',
+        id: 0
+      };
+    }
+    
+    onEdit(item){
+      this.task = item;
+    }
+
+    onDelete(item){
+      for(var i = 0;i < this.tasks.length; i++){
+        if(item.id == this.tasks[i].id){
+          this.tasks.splice(i,1);
+          break;
+        }
+      }
+    }
+
+    onStrike(item){
+      for(var i = 0;i < this.tasks.length; i++){
+        if(item.id == this.tasks[i].id){
+          if(this.tasks[i].strike){
+            this.tasks[i].strike = false;
+          }
+          else{
+            this.tasks[i].strike = true;
+          }
+          break;
+        }
+      }
+    }
 
   
 }
